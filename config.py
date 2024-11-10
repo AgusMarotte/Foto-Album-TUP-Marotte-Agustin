3876d28e85ebc534296909df564c0512210c6269
Agus <125620737+AgusMarotte@users.noreply.github.com>
2024-11-10 19:32:48 -0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = 'tp_python'
+    SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(BASE_DIR, "photo.db")}'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
